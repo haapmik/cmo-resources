a10359619d67b3454f4c29baefc56ea2f1ba72ea
Mikko Haapanen <26046991+haapmik@users.noreply.github.com>
2023-01-07 05:13:40 +0200

    Added Åland demilitarized zone

---
+++ finland/aaland-dmz.lua
@@ -0,0 +1,42 @@
+-- Åland convention of 1856 was signed by Russia after
+-- the Crimean War to agree not to militarise the Åland
+-- islands.
+--
+-- After Finland gained independency in 1918, another Åland
+-- convention was signed in 1921 by Finland, Sweden, Germany,
+-- United Kingdom, France, Italy, Denmark, Poland, Estonia,
+-- and Latvia.
+--
+-- Third convention was signed between Finland and Russia in 1940.
+--
+-- More information:
+--     - https://en.wikipedia.org/wiki/%C3%85land_Islands_dispute
+--     - https://en.wikipedia.org/wiki/%C3%85land_convention
+--
+-- Author: Mikko Haapanen <haapanen.mo@gmail.com>
+--
+-- SPDX-License-Identifier: MIT
+
+local side = "Finland"
+local zoneName = "Åland DMZ"
+
+-- East side
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.683333333, lon = 21.000000000, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.585833333, lon = 21.102500000, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.550833333, lon = 21.133333333, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.252222222, lon = 21.084722222, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.184444444, lon = 21.001111111, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.151111111, lon = 21.017222222, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.084722222, lon = 21.067500000, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.016944444, lon = 21.184166667, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 59.983333333, lon = 21.134166667, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 59.883333333, lon = 21.333333333, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 59.801388889, lon = 21.333333333, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 59.450000000, lon = 20.767500000, highlighted = true })
+
+-- West side
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 59.450000000, lon = 20.151944444, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 59.785555556, lon = 19.666666667, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.185555556, lon = 19.084722222, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.301111111, lon = 19.134722222, highlighted = true })
+ScenEdit_AddReferencePoint({ side = side, name = zoneName, lat = 60.683333333, lon = 19.234444444, highlighted = true })
